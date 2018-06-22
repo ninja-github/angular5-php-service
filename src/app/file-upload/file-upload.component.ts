8cabdcfaffa7599c7defbb4637ddc17b2e27b105
oa4ajp <mendiola_loyola@yahoo.com>
2018-06-22 14:54:45 -0500

    File Upload Spinner

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -2,16 +2,19 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FileUploadService } from '../service/file-upload.service';
 
 @Component({
-  templateUrl: './file-upload.component.html'
+  templateUrl: './file-upload.component.html',
+  styleUrls: ['./file-upload.component.css']
 })
 export class FileUploadComponent implements OnInit, OnDestroy {
   public fileToUpload: File = null;
   public uploadStatus: string = '';
+  public loading: boolean = false;
   
   constructor(private fileUploadService: FileUploadService) { }
 
   ngOnInit() {
     this.uploadStatus = '';
+    this.loading = false;
   }
   
   public ngOnDestroy(): void {
@@ -23,12 +26,18 @@ export class FileUploadComponent implements OnInit, OnDestroy {
   }
 
   public uploadFile() {
+    this.uploadStatus = '';
+    this.loading = true;
+
     this.fileUploadService.postFile(this.fileToUpload).subscribe(data => {
       // do something, if upload success
         this.uploadStatus = "File Upload successfully";
+        this.loading = false;
       }, error => {
         console.log(error);
-      });
+        this.loading = false;
+      }
+    );
   }
 
 }
