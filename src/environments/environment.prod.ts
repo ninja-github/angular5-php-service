c010400c186946792eee0c88e7927ca08e12abdc
oa4ajp <mendiola_loyola@yahoo.com>
2018-06-22 15:16:03 -0500

    File Upload Spinner

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://amendiola.co.nf/public',  
+  token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiQWxmcmVkbyBNZW5kaW9sYSJ9.NW7vVmu2OErfkeEIg0yg7dgfFCruM3BJSyWgH4x11Q8'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
-  apiUrl: 'http://amendiola.co.nf/public',
-  //apiUrl: 'http://127.0.0.1:8080/php-web-service-001/public',
+  production: false,  
+  apiUrl: 'http://127.0.0.1:8080/php-web-service-001/public',
   token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiQWxmcmVkbyBNZW5kaW9sYSJ9.NW7vVmu2OErfkeEIg0yg7dgfFCruM3BJSyWgH4x11Q8'
 };
